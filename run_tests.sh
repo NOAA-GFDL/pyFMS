315fcd80d79536b1dcdbdc818bf478e923a68ba3
Frank Malatino <frank.malatino@noaa.gov>
2025-05-07 14:17:41 -0400

    Trying all tests

--- run_tests.sh
+++ run_tests.sh
@@ -16,36 +16,36 @@ function run_test() {
 
 run_test "python -m pytest tests/test_build.py"
 
-# test="tests/test_pyfms.py"
-# create_input $test
-# run_test "python -m pytest -m parallel $test"
-# remove_input $test
+test="tests/test_pyfms.py"
+create_input $test
+run_test "python -m pytest -m parallel $test"
+remove_input $test
 
 test="tests/py_mpp/test_define_domains.py"
 create_input $test
 run_test "mpirun -n 8 --oversubscribe python -m pytest -m 'parallel' $test"
 remove_input $test
 
-# test="tests/py_mpp/test_getset_domains.py"
-# create_input $test
-# run_test "mpirun -n 4 python -m pytest -m 'parallel' tests/py_mpp/test_getset_domains.py"
-# remove_input $test
+test="tests/py_mpp/test_getset_domains.py"
+create_input $test
+run_test "mpirun -n 4 python -m pytest -m 'parallel' tests/py_mpp/test_getset_domains.py"
+remove_input $test
 
-# test="tests/py_mpp/test_update_domains.py"
-# create_input $test
-# run_test "mpirun -n 4 python -m pytest -m 'parallel' tests/py_mpp/test_update_domains.py"
-# remove_input $test
+test="tests/py_mpp/test_update_domains.py"
+create_input $test
+run_test "mpirun -n 4 python -m pytest -m 'parallel' tests/py_mpp/test_update_domains.py"
+remove_input $test
 
-# run_test "python -m pytest tests/py_horiz_interp"
+run_test "python -m pytest tests/py_horiz_interp"
 
-# run_test "python -m pytest tests/py_data_override/test_generate_files.py"
-# run_test "mpirun -n 6 python -m pytest -m 'parallel' tests/py_data_override/test_data_override.py"
-# remove_input "tests/py_data_override/test_data_override.py"
+run_test "python -m pytest tests/py_data_override/test_generate_files.py"
+run_test "mpirun -n 6 python -m pytest -m 'parallel' tests/py_data_override/test_data_override.py"
+remove_input "tests/py_data_override/test_data_override.py"
 
-# run_test "python -m pytest tests/py_diag_manager/test_generate_files.py"
-# run_test "mpirun -n 1 python -m pytest tests/py_diag_manager/test_diag_manager.py"
+run_test "python -m pytest tests/py_diag_manager/test_generate_files.py"
+run_test "mpirun -n 1 python -m pytest tests/py_diag_manager/test_diag_manager.py"
 
-# run_test "python -m pytest tests/utils/test_constants.py"
+run_test "python -m pytest tests/utils/test_constants.py"
 
 run_test "python -m pytest tests/test_init.py"
 
