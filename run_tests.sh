81e9821105f5c16d658bccfbafa78e0d0777ad1b
Frank Malatino <frank.malatino@noaa.gov>
2025-05-07 13:22:07 -0400

    Adding in test_define_domains

--- run_tests.sh
+++ run_tests.sh
@@ -21,10 +21,10 @@ run_test "python -m pytest tests/test_build.py"
 # run_test "python -m pytest -m parallel $test"
 # remove_input $test
 
-# test="tests/py_mpp/test_define_domains.py"
-# create_input $test
-# run_test "mpirun -n 8 python -m pytest -m 'parallel' $test"
-# remove_input $test
+test="tests/py_mpp/test_define_domains.py"
+create_input $test
+run_test "mpirun -n 8 python -m pytest -m 'parallel' $test"
+remove_input $test
 
 # test="tests/py_mpp/test_getset_domains.py"
 # create_input $test
