c50b9c497b26471c00eae3adbdad03c606ec98e1
MiKyung Lee <58964324+mlee03@users.noreply.github.com>
2025-10-09 09:54:59 -0400

    horiz_interp_base (#17)

--- pyfms/py_horiz_interp/_functions.py
+++ pyfms/py_horiz_interp/_functions.py
@@ -1,11 +1,11 @@
-from ctypes import POINTER, c_bool, c_char, c_int
+from ctypes import POINTER, c_bool, c_char, c_double, c_float, c_int
 
 import numpy as np
 
-from pyfms.utils.ctypes_utils import NDPOINTERd, NDPOINTERf
+from pyfms.utils.ctypes_utils import NDPOINTER
 
 
-npptr = np.ctypeslib.ndpointer
+ndpointer = np.ctypeslib.ndpointer
 C = "C_CONTIGUOUS"
 
 
@@ -25,17 +25,17 @@ def define(lib):
         POINTER(c_int),  # nlat_src
         POINTER(c_int),  # nlon_tgt
         POINTER(c_int),  # nlat_tgt
-        npptr(np.float64, ndim=1, flags=C),  # lon_src
-        npptr(np.float64, ndim=1, flags=C),  # lat_src
-        npptr(np.float64, ndim=1, flags=C),  # lon_tgt
-        npptr(np.float64, ndim=1, flags=C),  # lat_tgt
-        npptr(np.float64, ndim=1, flags=C),  # mask_src
+        ndpointer(np.float64, ndim=2, flags=C),  # lon_src
+        ndpointer(np.float64, ndim=2, flags=C),  # lat_src
+        ndpointer(np.float64, ndim=2, flags=C),  # lon_tgt
+        ndpointer(np.float64, ndim=2, flags=C),  # lat_tgt
+        ndpointer(np.float64, ndim=2, flags=C),  # mask_src
         POINTER(c_int),  # maxxgrid
-        npptr(np.int32, ndim=1, flags=C),  # i_src
-        npptr(np.int32, ndim=1, flags=C),  # j_src
-        npptr(np.int32, ndim=1, flags=C),  # i_tgt
-        npptr(np.int32, ndim=1, flags=C),  # j_tgt
-        npptr(np.float64, ndim=1, flags=C),  # xarea
+        ndpointer(np.int32, ndim=1, flags=C),  # i_src
+        ndpointer(np.int32, ndim=1, flags=C),  # j_src
+        ndpointer(np.int32, ndim=1, flags=C),  # i_tgt
+        ndpointer(np.int32, ndim=1, flags=C),  # j_tgt
+        ndpointer(np.float64, ndim=1, flags=C),  # xarea
     ]
 
     # get_maxxgrid
@@ -46,64 +46,77 @@ def define(lib):
     lib.cFMS_horiz_interp_init.restype = None
     lib.cFMS_horiz_interp_init.argtypes = [POINTER(c_int)]
 
-    # cFMS_horiz_interp_2d (for floats and doubles)
-    lib.cFMS_horiz_interp_new_2d_cdouble.restype = c_int
-    lib.cFMS_horiz_interp_new_2d_cdouble.argtypes = [
-        npptr(np.float64, ndim=2, flags=C),  # lon_in_ptr
-        npptr(np.float64, ndim=2, flags=C),  # lat_in_ptr
-        npptr(np.int32, ndim=1, flags=C),  # lonlat_in_shape
-        npptr(np.float64, ndim=2, flags=C),  # lon_out_ptr
-        npptr(np.float64, ndim=2, flags=C),  # lat_out_ptr
-        npptr(np.int32, ndim=1, flags=C),  # lonlat_out_shape
-        NDPOINTERd(npptr(np.float64, ndim=2, flags=C)),  # mask_in_ptr
-        NDPOINTERd(npptr(np.float64, ndim=2, flags=C)),  # mask_out_ptr
-        POINTER(c_char),  # interp_method
-        POINTER(c_int),  # verbose
-        NDPOINTERd(npptr(np.float64, ndim=1, flags=C)),  # max_dist
-        POINTER(c_bool),  # src_modulo
-        POINTER(c_bool),  # is_latlon_in
-        POINTER(c_bool),  # is_latlon_out
-    ]
+    # cFMS_horiz_interp_end
+    lib.cFMS_horiz_interp_end.restype = None
+    lib.cFMS_horiz_interp_end.argtypes = None
 
     # cFMS_horiz_interp_2d (for floats and doubles)
-    lib.cFMS_horiz_interp_new_2d_cfloat.restype = c_int
-    lib.cFMS_horiz_interp_new_2d_cfloat.argtypes = [
-        npptr(np.float32, ndim=2, flags=C),  # lon_in_ptr
-        npptr(np.float32, ndim=2, flags=C),  # lat_in_ptr
-        npptr(np.int32, ndim=1, flags=C),  # lonlat_in_shape
-        npptr(np.float32, ndim=2, flags=C),  # lon_out_ptr
-        npptr(np.float32, ndim=2, flags=C),  # lat_out_ptr
-        npptr(np.int32, ndim=1, flags=C),  # lonlat_out_shape
-        NDPOINTERf(npptr(np.float32, ndim=2, flags=C)),  # mask_in_ptr
-        NDPOINTERf(npptr(np.float32, ndim=2, flags=C)),  # mask_out_ptr
-        POINTER(c_char),  # interp_method
-        POINTER(c_int),  # verbose
-        NDPOINTERf(npptr(np.float32, ndim=1, flags=C)),  # max_dist
-        POINTER(c_bool),  # src_modulo
-        POINTER(c_bool),  # is_latlon_in
-        POINTER(c_bool),  # is_latlon_out
+    news = [
+        (np.float32, lib.cFMS_horiz_interp_new_2d_cfloat),
+        (np.float64, lib.cFMS_horiz_interp_new_2d_cdouble),
     ]
+    for np_float, cFMS_horiz_interp_new in news:
+        cFMS_horiz_interp_new.restype = c_int
+        cFMS_horiz_interp_new.argtypes = [
+            POINTER(c_int),  # nlon_in
+            POINTER(c_int),  # nlat_in
+            POINTER(c_int),  # nlon_out
+            POINTER(c_int),  # nlat_out
+            ndpointer(np_float, ndim=2, flags=C),  # lon_in_ptr
+            ndpointer(np_float, ndim=2, flags=C),  # lat_in_ptr
+            ndpointer(np_float, ndim=2, flags=C),  # lon_out_ptr
+            ndpointer(np_float, ndim=2, flags=C),  # lat_out_ptr
+            NDPOINTER(np_float, ndim=2, flags=C),  # mask_in_ptr
+            NDPOINTER(np_float, ndim=2, flags=C),  # mask_out_ptr
+            POINTER(c_char),  # interp_method
+            POINTER(c_int),  # verbose
+            NDPOINTER(np_float, ndim=1, flags=C),  # max_dist
+            POINTER(c_bool),  # src_modulo
+            POINTER(c_bool),  # is_latlon_in
+            POINTER(c_bool),  # is_latlon_out
+            POINTER(c_bool),  # convert_cf_order
+        ]
+
+    # cFMS_horiz_interp_base_2d
+    bases = [
+        (np.float32, c_float, lib.cFMS_horiz_interp_base_2d_cfloat),
+        (np.float64, c_double, lib.cFMS_horiz_interp_base_2d_cdouble),
+    ]
+    for np_float, c_real, cFMS_horiz_interp_base in bases:
+        cFMS_horiz_interp_base.restype = None
+        cFMS_horiz_interp_base.argtypes = [
+            POINTER(c_int),  # interp_id
+            ndpointer(np_float, ndim=2, flags=C),  # data_in_ptr
+            ndpointer(np_float, ndim=2, flags=C),  # data_out_ptr
+            NDPOINTER(np_float, ndim=2, flags=C),  # mask_in
+            NDPOINTER(np_float, ndim=2, flags=C),  # mask_out
+            POINTER(c_int),  # verbose
+            POINTER(c_real),  # missing_value
+            POINTER(c_int),  # missing_permit
+            POINTER(c_bool),  # new_missing_handle
+            POINTER(c_bool),  # convert_cf_order
+        ]
 
     # getter routines for individual fields
     lib.cFMS_get_wti_cfloat.restype = None
     lib.cFMS_get_wti_cfloat.argtypes = [
         POINTER(c_int),
-        npptr(np.float32, ndim=3, flags=C),
+        ndpointer(np.float32, ndim=3, flags=C),
     ]
     lib.cFMS_get_wti_cdouble.restype = None
     lib.cFMS_get_wti_cdouble.argtypes = [
         POINTER(c_int),
-        npptr(np.float64, ndim=3, flags=C),
+        ndpointer(np.float64, ndim=3, flags=C),
     ]
     lib.cFMS_get_wtj_cfloat.restype = None
     lib.cFMS_get_wtj_cfloat.argtypes = [
         POINTER(c_int),
-        npptr(np.float32, ndim=3, flags=C),
+        ndpointer(np.float32, ndim=3, flags=C),
     ]
     lib.cFMS_get_wtj_cdouble.restype = None
     lib.cFMS_get_wtj_cdouble.argtypes = [
         POINTER(c_int),
-        npptr(np.float64, ndim=3, flags=C),
+        ndpointer(np.float64, ndim=3, flags=C),
     ]
 
     # cFMS_get_interp_method
@@ -112,25 +125,25 @@ def define(lib):
 
     # cFMS_get_i_src
     lib.cFMS_get_i_src.restype = None
-    lib.cFMS_get_i_src.argtypes = [POINTER(c_int), npptr(np.int32, ndim=1, flags=C)]
+    lib.cFMS_get_i_src.argtypes = [POINTER(c_int), ndpointer(np.int32, ndim=1, flags=C)]
 
     # cFMS_get_j_src
     lib.cFMS_get_j_src.restype = None
-    lib.cFMS_get_j_src.argtypes = [POINTER(c_int), npptr(np.int32, ndim=1, flags=C)]
+    lib.cFMS_get_j_src.argtypes = [POINTER(c_int), ndpointer(np.int32, ndim=1, flags=C)]
 
     # cFMS_get_i_dst
     lib.cFMS_get_i_dst.restype = None
-    lib.cFMS_get_i_dst.argtypes = [POINTER(c_int), npptr(np.int32, ndim=1, flags=C)]
+    lib.cFMS_get_i_dst.argtypes = [POINTER(c_int), ndpointer(np.int32, ndim=1, flags=C)]
 
     # cFMS_get_j_dst
     lib.cFMS_get_j_dst.restype = None
-    lib.cFMS_get_j_dst.argtypes = [POINTER(c_int), npptr(np.int32, ndim=1, flags=C)]
+    lib.cFMS_get_j_dst.argtypes = [POINTER(c_int), ndpointer(np.int32, ndim=1, flags=C)]
 
     # cFMS_get_area_frac_dst
     lib.cFMS_get_area_frac_dst_cdouble.restype = None
     lib.cFMS_get_area_frac_dst_cdouble.argtypes = [
         POINTER(c_int),
-        npptr(np.float64, ndim=1, flags=C),
+        ndpointer(np.float64, ndim=1, flags=C),
     ]
 
     # cFMS_get_nlon_src
--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -10,12 +10,12 @@ while getopts ":o" flag; do
 done
 
 function create_input() {
-    pytest -m "create" $1
+    pytest -svm "create" $1
     if [ $? -ne 0 ] ; then exit 1 ; fi
 }
 
 function remove_input() {
-    pytest -m "remove" $1
+    pytest -svm "remove" $1
     if [ $? -ne 0 ] ; then exit 1 ; fi
 }
 
@@ -26,52 +26,55 @@ function run_test() {
 
 test="test_fms.py"
 create_input $test
-run_test "python -m pytest -m parallel $test"
+run_test "python -m pytest -svm parallel $test"
 remove_input $test
 
 test="py_mpp/test_define_domains.py"
 create_input $test
-run_test "mpirun -n 8 $oversubscribe python -m pytest -m 'parallel' $test"
+run_test "mpirun -n 8 $oversubscribe python -m pytest -svm 'parallel' $test"
 remove_input $test
 
 test="py_mpp/test_define_cubic_mosaic.py"
 create_input $test
-run_test "mpirun -n 30 $oversubscribe python -m pytest -m 'parallel' $test"
+run_test "mpirun -n 30 $oversubscribe python -m pytest -svm 'parallel' $test"
 remove_input $test
 
 test="py_mpp/test_getset_domains.py"
 create_input $test
-run_test "mpirun -n 4 $oversubscribe python -m pytest -m 'parallel' py_mpp/test_getset_domains.py"
+run_test "mpirun -n 4 $oversubscribe python -m pytest -svm 'parallel' py_mpp/test_getset_domains.py"
 remove_input $test
 
 test="py_mpp/test_update_domains.py"
 create_input $test
-run_test "mpirun -n 4 $oversubscribe python -m pytest -m 'parallel' py_mpp/test_update_domains.py"
+run_test "mpirun -n 4 $oversubscribe python -m pytest -svm 'parallel' py_mpp/test_update_domains.py"
 remove_input $test
 
 test="py_mpp/test_vector_update_domains.py"
 create_input $test
-run_test "mpirun -n 2 $oversubscribe python -m pytest -m 'parallel' py_mpp/test_vector_update_domains.py"
+run_test "mpirun -n 2 $oversubscribe python -m pytest -svm 'parallel' py_mpp/test_vector_update_domains.py"
 remove_input $test
 
 test="py_horiz_interp/test_horiz_interp.py"
 create_input $test
 run_test "python -m pytest -sv -k test_create_xgrid $test"
-run_test "python -m pytest -sv -k test_horiz_interp_conservative $test"
-run_test "mpirun -n 4 $oversubscribe python -m pytest -s -k test_horiz_interp_conservative $test"
+run_test "python -svm pytest -sv -k test_horiz_interp_conservative $test"
+run_test "mpirun -n 4 $oversubscribe python -m pytest -sv -k test_horiz_interp_conservative $test"
 run_test "python -m pytest -s -k test_horiz_interp_bilinear $test"
-run_test "mpirun -n 2 $oversubscribe python -m pytest -s -k test_horiz_interp_bilinear $test"
+run_test "mpirun -n 2 $oversubscribe python -m pytest -sv -k test_horiz_interp_bilinear $test"
 remove_input $test
 
-run_test "python -m pytest py_data_override/test_generate_files.py"
-run_test "mpirun -n 6 $oversubscribe python -m pytest -m 'parallel' py_data_override/test_data_override.py"
-remove_input "py_data_override/test_data_override.py"
+#test temporarily turned off on Github Action
+if [ ! -n $oversubscribe ] ; then
+    run_test "python -m pytest py_data_override/test_generate_files.py"
+    run_test "mpirun -n 6 $oversubscribe python -m pytest -svm 'parallel' py_data_override/test_data_override.py"
+    remove_input "py_data_override/test_data_override.py"
+fi
 
 run_test "python -m pytest py_diag_manager/test_generate_files.py"
-run_test "mpirun -n 1 python -m pytest py_diag_manager/test_diag_manager.py"
+run_test "mpirun -n 1 python -svm pytest py_diag_manager/test_diag_manager.py"
 
-run_test "python -m pytest utils/test_constants.py"
+run_test "python -svm pytest utils/test_constants.py"
 
-run_test "python -m pytest test_init.py"
+run_test "python -svm pytest test_init.py"
 
 rm -rf INPUT *logfile* *warnfile*
