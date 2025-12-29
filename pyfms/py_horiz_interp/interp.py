bf589f7423cdbf2d393e548281e2de019e484c0b
mlee03 <Mikyung.Lee@lscamd50-d.gfdl.noaa.gov>
2025-12-29 11:57:28 -0500

    add repr

--- pyfms/py_horiz_interp/interp.py
+++ pyfms/py_horiz_interp/interp.py
@@ -1,7 +1,9 @@
+import numpy as np
 from pyfms.py_horiz_interp import horiz_interp
 
 
 class Interp:
+
     def __init__(self, interp_id: int):
         # set only for conservative
         self.interp_id = interp_id
@@ -15,4 +17,23 @@ class Interp:
         self.nlon_dst = horiz_interp.get_nlon_dst(interp_id)
         self.nlat_dst = horiz_interp.get_nlat_dst(interp_id)
         self.interp_method = horiz_interp.get_interp_method(interp_id)
-        self.get_area_frac_dst = horiz_interp.get_area_frac_dst(interp_id)
+        self.area_frac_dst = horiz_interp.get_area_frac_dst(interp_id)
+
+    def __repr__(self):
+
+        repr_str = f"""
+            interp_id: {self.interp_id}
+            nxgrid: {self.nxgrid}
+            nlon_src: {self.nlon_src}
+            nlat_src: {self.nlat_src}
+            nlon_dst: {self.nlon_dst}
+            nlat_dst: {self.nlat_dst}
+            interp_method: {self.interp_method}
+            i_src_minmax: [{self.i_src.min()}, {self.i_src.max()}]
+            j_src_minmax [{self.j_src.min()}, {self.j_src.max()}]
+            i_dst_minmax: [{self.i_dst.min()}, {self.i_dst.max()}]
+            j_dst_minmax: [{self.j_dst.min()}, {self.j_dst.max()}]
+            area_frac_dst_minmax: [{self.area_frac_dst.min()}, {self.area_frac_dst.max()}]
+        """
+
+        return repr_str
