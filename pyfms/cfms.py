691d3be971e87b20031e01eee28d4b2a2e311f59
Frank Malatino <frank.malatino@noaa.gov>
2026-03-13 15:02:56 -0400

    Pre-lim work to use cmake install of cfms

--- pyfms/cfms.py
+++ pyfms/cfms.py
@@ -1,5 +1,5 @@
 import ctypes
-
+from pathlib import Path
 import pyfms
 
 
@@ -25,7 +25,7 @@ def init(libpath: str = None):
     # todo reset all _function parameters to None
 
     if libpath is None:
-        _libpath = "libcFMS.so"
+        _libpath = "/home/Frank.Malatino/.conda/envs/throw3/lib/python3.11/site-packages/pyfms/lib/libcFMS.so"
         try:
             _lib = ctypes.cdll.LoadLibrary(_libpath)
         except Exception as e:
